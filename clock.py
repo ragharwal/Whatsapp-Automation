ae28af2d0e9754de439f28c6f7091fd9b0d2c11e
Raghav Agarwal <ragharwal@gmail.com>
2020-12-27 18:24:41 +0530

    '1.1.0'

--- clock.py
+++ clock.py
@@ -2,5 +2,5 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from whatsappAutoMsgSender import sendMsg
 
 sched = BlockingScheduler()
-sched.add_job(sendMsg, 'interval', seconds=5)
+sched.add_job(sendMsg, 'interval', seconds=3600)
 sched.start()
